cc74802dd23d809495feb2fb511a11be8a4fbe05
AlxisHenry <alexis.henry150357@gmail.com>
2022-03-28 15:59:23 +0200

    Ajout d'un calcul du temps entre la dernière connection et l'actuelle

--- admin-panel.php
+++ admin-panel.php
@@ -1,23 +1,62 @@
 <!DOCTYPE html>
 <html>
+<?php
 
+include 'src/php/functions.php';
+
+$GetDateOfLastConnexion = "SELECT `derniereConnection` AS 'DATE' FROM `panel_manage_access` WHERE `username` = 'tfadmin'";;
+$DB_QUERY = Connection()->query($GetDateOfLastConnexion);
+$lastConnection = $DB_QUERY->fetch();
+$DB_QUERY->closeCursor();
+
+$today = date("Y-m-d H:i:s");
+$lastConnection = $lastConnection['DATE'];
+
+$today = new DateTime($today);
+$lastConnection = new DateTime($lastConnection);
+$difference = $today->diff($lastConnection);
+
+if (($difference->m) == 0) {
+
+    if (($difference->d) == 0) {
+
+        if (($difference->i) == 0) {
+
+            if (($difference->s) == 0) {
+
+            } else {
+                $dateToShow= $difference->s . ' secondes';
+            }
+
+        } else {
+            $dateToShow = $difference->i . ' minutes';
+        }
+
+    } else {
+        $dateToShow = $difference->d . ' days';
+    }
+
+} else {
+    $dateToShow = $difference->m . ' months';
+}
+
+?>
 <head>
     <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <meta http-equiv='content-type' content='text/html' charset='utf-8' />
     <title>Login (test)</title>
-    <script src="https://kit.fontawesome.com/2d2078daec.js" crossorigin="anonymous"></script>
-    <link rel='icon' href='https://1ja0pa1tvpl63v04fj2l0oby-wpengine.netdna-ssl.com/wp-content/themes/timken/favicon.ico'>
-    <link href='./src/styles/main.css' media='all' rel='stylesheet' type='text/css' />
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link rel='icon' href='./assets/favicon.ico'>
+    <link href='./src/css/main.css' media='all' rel='stylesheet' type='text/css' />
 </head>
 
 <body>
 
-<div class="switch-dashboard"><div class="switch-indicator"></div></div>
+<div class="">Dernière connection il y a <?= $dateToShow ?></div>
 
 <script src='./src/js/jquery-3.6.0.min.js'></script>
-<script src='./src/js/admin-panel.js' type="module"></script>
+<script src='./src/js/index.js' type="module"></script>
 
 </body>
 
---
+++ src/php/configuration/sql/timken_test.sql
@@ -0,0 +1,6 @@
+DROP DATABASE
+    IF EXISTS scrapping;
+CREATE DATABASE scrapping
+    DEFAULT CHARACTER SET utf8mb4
+    COLLATE utf8mb4_general_ci;
+USE scrapping;
