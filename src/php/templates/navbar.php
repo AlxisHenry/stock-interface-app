46868b3a42409c52424c02d8dd36d6f5ad219ed2
AlxisHenry <alexis.henry150357@gmail.com>
2022-03-29 13:37:07 +0200

    Ajout templates

--- src/php/templates/navbar.php
+++ src/php/templates/navbar.php
@@ -1,26 +1,26 @@
 <main class="features-navigation">
 
     <div class="logo">
-        <img src="./../../../assets/images/Timken.png">
+        <img src="../../../assets/images/Timken.png">
     </div>
 
     <div class="contain-features">
-        <div class="features last-mouvements"><a class="nav-redirection" href="./mouvemenets"><i class="nav-fa-icons fa-solid fa-arrow-trend-up"></i><span class="nav-span">Derniers mouvements</span></a></div>
-        <div class="features stock-entry"><a class="nav-redirection" href=""><i class="nav-fa-icons fa-solid fa-boxes-stacked"></i><span class="nav-span">Entrée de stock</span></a></div>
-        <div class="features stock-checkout"><a class="nav-redirection" href=""><i class="nav-fa-icons fa-solid fa-dolly"></i><span class="nav-span">Sortie de stock</span></a></div>
-        <div class="features visualisation"><a class="nav-redirection" href=""><i class="nav-fa-icons fa-solid fa-box-open"></i><span class="nav-span">Voir le stock</span></a></div>
-        <div class="features config-users"><a class="nav-redirection" href=""><i class="nav-fa-icons fa-solid fa-user-gear"></i><span class="nav-span">Configurer Utilisateurs</span></a></div>
-        <div class="features config-article"><a class="nav-redirection" href=""><i class="nav-fa-icons fa-solid fa-user-gear"></i><span class="nav-span">Configurer Articles</span></a></div>
-        <div class="features config-famille"><a class="nav-redirection" href=""><i class="nav-fa-icons fa-solid fa-user-gear"></i><span class="nav-span">Configurer Familles</span></a></div>
-        <div class="features config-ccout"><a class="nav-redirection" href=""><i class="nav-fa-icons fa-solid fa-user-gear"></i><span class="nav-span">Configurer C. Coût</span></a></div>
+        <div class="features mvmt last-mouvements"><a class="nav-redirection" href="./dashboard.php?nav=mvmt"><i class="nav-fa-icons fa-solid fa-arrow-trend-up"></i><span class="nav-span">Derniers mouvements</span></a></div>
+        <div class="features s-entry stock-entry"><a class="nav-redirection" href="./stock_in.php?nav=s-entry"><i class="nav-fa-icons fa-solid fa-boxes-stacked"></i><span class="nav-span">Entrée de stock</span></a></div>
+        <div class="features s-checkout stock-checkout"><a class="nav-redirection" href="./stock_out.php?nav=s-checkout"><i class="nav-fa-icons fa-solid fa-dolly"></i><span class="nav-span">Sortie de stock</span></a></div>
+        <div class="features visu visualisation"><a class="nav-redirection" href="./stock.php?nav=visu"><i class="nav-fa-icons fa-solid fa-box-open"></i><span class="nav-span">Voir le stock</span></a></div>
+        <div class="features c-users config-users"><a class="nav-redirection" href="./configs.php?nav=c-users"><i class="nav-fa-icons fa-solid fa-user-gear"></i><span class="nav-span">Configurer Utilisateurs</span></a></div>
+        <div class="features c-article config-article"><a class="nav-redirection" href="./configs.php?nav=c-article"><i class="nav-fa-icons fa-solid fa-user-gear"></i><span class="nav-span">Configurer Articles</span></a></div>
+        <div class="features c-famille config-famille"><a class="nav-redirection" href="./configs.php?nav=c-famille"><i class="nav-fa-icons fa-solid fa-user-gear"></i><span class="nav-span">Configurer Familles</span></a></div>
+        <div class="features c-ccout config-ccout"><a class="nav-redirection" href="./configs.php?nav=c-ccout"><i class="nav-fa-icons fa-solid fa-user-gear"></i><span class="nav-span">Configurer C. Coût</span></a></div>
     </div>
 
     <hr class="nav-separator">
 
     <div class="contain-actions">
-        <div class="features alerts"><a class="nav-redirection" href=""><i class="nav-fa-icons fa-solid fa-bell"></i><span class="nav-span">Notifications</span></a></div> <!-- Pop up qui apparaît juste à côté avec le nombres ? Si 0 > rien, sinon la valeur ?-->
-        <div class="features configurations"><a class="nav-redirection" href=""><i class="nav-fa-icons fa-solid fa-gear"></i><span class="nav-span">Settings</span></a></div>
-        <div class="features disconnect"><a class="nav-redirection" href=""><i class="nav-fa-icons fa-solid fa-right-from-bracket"></i><span class="nav-span">Me déconnecter</span></a></div>
+        <div class="features alerts"><a class="nav-redirection" href="./alerts.php?nav=alerts"><i class="nav-fa-icons fa-solid fa-bell"></i><span class="nav-span">Notifications</span></a></div> <!-- Pop up qui apparaît juste à côté avec le nombres ? Si 0 > rien, sinon la valeur ?-->
+        <div class="features settings"><a class="nav-redirection" href="./settings.php?nav=settings"><i class="nav-fa-icons fa-solid fa-gear"></i><span class="nav-span">Settings</span></a></div>
+        <div class="features disconnect"><a class="nav-redirection" href="#"><i class="nav-fa-icons fa-solid fa-right-from-bracket"></i><span class="nav-span">Me déconnecter</span></a></div>
     </div>
 
 </main>
