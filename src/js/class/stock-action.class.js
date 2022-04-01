0862f9f06137fef5c61014b8cf9ebc31f9d7801f
AlxisHenry <alexis.henry150357@gmail.com>
2022-04-01 11:15:26 +0200

    Ajout d'un title dynamique pour les icones d'action sur le tableau de stock.

--- src/js/class/stock-action.class.js
+++ src/js/class/stock-action.class.js
@@ -1,19 +1,59 @@
 import {consoleLog} from "../global/app.js";
 
-export class StockActionClass {
+export class Stock {
 
     constructor() {
         this.ActionArticleValues = [];
+        this.ParentElement = '';
     };
+
+    InitializeStockTitles() {
+
+       const ActionNodeList = [document.querySelectorAll('.entry'), document.querySelectorAll('.checkout'),  document.querySelectorAll('.edit')]
+
+        ActionNodeList.forEach(ActionType =>
+            ActionType.forEach(Action => {
+                Action.addEventListener('mouseover', (e) => {
+                    switch (e.target.classList[3]) {
+                        case 'entry':
+                            this.ActionArticleValues = [];
+                            this.ParentElement = e.target.parentNode.parentNode;
+                            for (let Element = 0; Element < this.ParentElement.childNodes.length; Element++) {
+                                this.ActionArticleValues.push(this.ParentElement.childNodes[Element].innerText);
+                            }
+                            e.target.title = 'Entrée de stock pour ' + this.ActionArticleValues[2];
+                            break;
+                        case 'checkout':
+                            this.ActionArticleValues = [];
+                            this.ParentElement = e.target.parentNode.parentNode;
+                            for (let Element = 0; Element < this.ParentElement.childNodes.length; Element++) {
+                                this.ActionArticleValues.push(this.ParentElement.childNodes[Element].innerText);
+                            }
+                            e.target.title = 'Sortie de stock pour ' + this.ActionArticleValues[2];
+                            break;
+                        case 'edit':
+                            this.ActionArticleValues = [];
+                            this.ParentElement = e.target.parentNode.parentNode;
+                            for (let Element = 0; Element < this.ParentElement.childNodes.length; Element++) {
+                                this.ActionArticleValues.push(this.ParentElement.childNodes[Element].innerText);
+                            }
+                            e.target.title = 'Editer ' + this.ActionArticleValues[2];
+                            break;
+                        default:
+                            e.target.title = 'Gestion du stock de cet article';
+                    }
+                });
+            })
+        );
+    }
+
 
     ActionEntry(e) {
 
-        const ParentElement = e.target.parentNode.parentNode;
-        const TargetAllElements = ParentElement.childNodes;
-        this.ActionArticleValues.push(e.target.classList[3]);
-
-        for (let Element = 0; Element < TargetAllElements.length; Element++) {
-            this.ActionArticleValues.push(TargetAllElements[Element].innerText);
+        this.ActionArticleValues = [];
+        this.ParentElement = e.target.parentNode.parentNode;
+        for (let Element = 0; Element < this.ParentElement.childNodes.length; Element++) {
+            this.ActionArticleValues.push(this.ParentElement.childNodes[Element].innerText);
         }
 
         console.table(this.ActionArticleValues);
@@ -22,12 +62,10 @@ export class StockActionClass {
 
     ActionCheckout(e) {
 
-        const ParentElement = e.target.parentNode.parentNode;
-        const TargetAllElements = ParentElement.childNodes;
-        this.ActionArticleValues.push(e.target.classList[3]);
-
-        for (let Element = 0; Element < TargetAllElements.length; Element++) {
-            this.ActionArticleValues.push(TargetAllElements[Element].innerText);
+        this.ActionArticleValues = [];
+        this.ParentElement = e.target.parentNode.parentNode;
+        for (let Element = 0; Element < this.ParentElement.childNodes.length; Element++) {
+            this.ActionArticleValues.push(this.ParentElement.childNodes[Element].innerText);
         }
 
         console.table(this.ActionArticleValues);
@@ -36,15 +74,61 @@ export class StockActionClass {
 
     ActionEdit(e) {
 
-        const ParentElement = e.target.parentNode.parentNode;
-        const TargetAllElements = ParentElement.childNodes;
-        this.ActionArticleValues.push(e.target.classList[3]);
-
-        for (let Element = 0; Element < TargetAllElements.length; Element++) {
-            this.ActionArticleValues.push(TargetAllElements[Element].innerText);
+        this.ActionArticleValues = [];
+        this.ParentElement = e.target.parentNode.parentNode;
+        for (let Element = 0; Element < this.ParentElement.childNodes.length; Element++) {
+            this.ActionArticleValues.push(this.ParentElement.childNodes[Element].innerText);
         }
 
         console.table(this.ActionArticleValues);
 
     }
+
+    ResponsiveColumns() {
+
+        const AllTableTitles = document.querySelectorAll('.column-title');
+
+        AllTableTitles.forEach(Title => {
+            switch (Title.innerHTML) {
+                case 'Dernière modification':
+                    RemoveColumns(Title, 1450);
+                    break;
+                case 'Commentaire':
+                    RemoveColumns(Title, 1450);
+                    break;
+                case 'Numéro':
+                    RemoveColumns(Title, 1450);
+                    break;
+                case 'Famille':
+                    RemoveColumns(Title, 1450);
+                    break;
+                case 'Code':
+                    RemoveColumns(Title, 900);
+                    break;
+                case 'Localisation':
+                    RemoveColumns(Title, 900);
+                    break;
+            }
+        })
+
+        function RemoveColumns(value, step) {
+            if(window.matchMedia("(max-width:"+ step +"px)").matches) {
+                document.querySelectorAll(`.${value.classList[0]}`).forEach(Value => {
+                    Value.classList.add('d-none');
+                })
+            } else {
+                document.querySelectorAll(`.${value.classList[0]}`).forEach(Value => {
+                    Value.classList.remove('d-none');
+                })
+            }
+        }
+
+    }
+
+    RemoveLastColumn() {
+        const AllColumns = document.querySelectorAll('.row-values');
+        AllColumns[AllColumns.length -1].remove();
+    }
+
+
 }
--- src/js/main/stock.js
+++ src/js/main/stock.js
@@ -1,25 +1,25 @@
-import { RemoveLastColumn, ResponsiveColumns } from "../global/app.js";
-import { StockActionClass } from "../class/stock-action.class.js";
+import { Stock } from "../class/stock-action.class.js";
 
 window.addEventListener('load', () => {
 
-    RemoveLastColumn();
-    ResponsiveColumns();
+    new Stock().RemoveLastColumn();
+    new Stock().ResponsiveColumns();
+    new Stock().InitializeStockTitles();
 
     window.addEventListener('resize', () => {
-            ResponsiveColumns();
+        new Stock().ResponsiveColumns();
     })
 
     document.querySelectorAll('.entry').forEach(Entry => Entry.addEventListener('click', (e) => {
-        new StockActionClass().ActionEntry(e);
+        new Stock().ActionEntry(e);
     }));
 
     document.querySelectorAll('.checkout').forEach(Entry => Entry.addEventListener('click', (e) => {
-        new StockActionClass().ActionCheckout(e);
+        new Stock().ActionCheckout(e);
     }));
 
     document.querySelectorAll('.edit').forEach(Entry => Entry.addEventListener('click', (e) => {
-        new StockActionClass().ActionEdit(e);
+        new Stock().ActionEdit(e);
     }));
 
 
