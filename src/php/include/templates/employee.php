d75da519b5adc0f72f11ada8f795b7507da716e3
AlxisHenry <alexis.henry150357@gmail.com>
2022-04-25 08:54:21 +0200

    Template employee

--- src/php/include/templates/employee.php
+++ src/php/include/templates/employee.php
@@ -1,1 +1,8 @@
 <?php
+include '../../functions.php';
+CheckSessionExist();
+CheckSessionAccount();
+?>
+<!DOCTYPE html>
+<html lang="FR">
+
--- src/php/include/views/view.php
+++ src/php/include/views/view.php
@@ -1,13 +1,1 @@
-<?php
-include '../../functions.php';
-
-if (!CheckSessionExist('login')) { header("Location: ../../../../index.php"); die(); };
-if (GetSession('login', 'typeAccount') !== 'view') { header("Location: ../../../../index.php"); die(); }
-
-var_dump($_SESSION);
-
-?>
-
-<html>
-
-</html>
+<?php include '../templates/employee.php';
