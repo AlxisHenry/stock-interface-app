5b8f1c0ed379fc527619edd16dda21d9a5c2ccea
AlxisHenry <alexis.henry150357@gmail.com>
2022-05-23 15:39:07 +0200

    Fix: css table

--- src/js/class/stocks/stock_out.class.js
+++ src/js/class/stocks/stock_out.class.js
@@ -15,4 +15,6 @@ export class stock_out extends Stock {
     }
 
 
+
+
 }
--- src/php/ajax/request-stock-research.php
+++ src/php/ajax/request-stock-research.php
@@ -13,7 +13,7 @@ OR `articles`.localisation LIKE '%$VALUE%'
 OR `familles`.nom LIKE '%$VALUE%'
 OR `articles`.dateModification LIKE '%$VALUE%';");
 
-$i = 0;
+$i = 1;
 
 while ($STOCK = $GET_STOCK->fetch()) {
     $FORMAT_DATE = date('d/m/Y, H:i', strtotime($STOCK['dateModification']));
@@ -30,8 +30,8 @@ while ($STOCK = $GET_STOCK->fetch()) {
             <td class='column-8 column-values action'><a class='redirect-entry' href='./stock_in.php?nav=s-entry&id=".$STOCK['id']."'><i title='Entrée de stock pour ".$STOCK['nom']."' class='fa-solid fa-plus action entry'></i></a></td>
             <td class='column-9 column-values action'><a class='redirect-out' href='./stock_out.php?nav=s-checkout&id=".$STOCK['id']."'><i title='Sortie de stocc pour ".$STOCK['nom']."' class='fa-solid fa-minus action checkout'></a></td>
             <td class='column-10 column-values action'><a class='redirect-edit' href='./config-articles.php?nav=c-article&id=".$STOCK['id']."'><i title='Editer ".$STOCK['nom']."' class='fa-solid fa-pen-clip action edit'></i></a></td>
-            </tr>\n</tr><tr class='row-". ($i + 1) . " row-values'>";
-    $i = $i + 2;
+            </tr>\n";
+    $i = $i + 1;
 }
 
 $GET_STOCK->closeCursor();
