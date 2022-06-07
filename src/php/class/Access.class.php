44993f6f7fa6dfe283935cb13c9650f13f3330be
AlxisHenry <alexis.henry150357@gmail.com>
2022-06-07 09:13:19 +0200

    Add Updates functions for password, datetime

--- src/php/class/Access.class.php
+++ src/php/class/Access.class.php
@@ -191,4 +191,40 @@ class Access
         $this->EditUser = $EditUser;
     }
 
+    public function Update(string $username, string $pwd, string $type, $derniereConnection, string $commentaire, int $editUser, int $id) {
+        $REQUEST = 'UPDATE `access` SET username = :username, password = :pwd, type = :type, derniereConnection = :derniereConnection, commentaire = :commentaire, dateModification = (SELECT NOW()), editUser = :editUser WHERE `id` = :id';
+        $QUERY = Connection()->prepare($REQUEST);
+        $QUERY->execute([
+            'username' => $username,
+            'pwd' => $pwd,
+            'type' => $type,
+            'derniereConnection' => $derniereConnection,
+            'commentaire' => $commentaire,
+            'editUser' => $editUser,
+            'id' => $id
+        ]);
+        $QUERY->closeCursor();
+    }
+
+    public function UpdatePassword(string $password,int $editUser,int $user) {
+        $REQUEST = 'UPDATE `access` SET password = :pwd, dateModification = (SELECT NOW()), editUser = :editUser WHERE `id` = :id';
+        $QUERY = Connection()->prepare($REQUEST);
+        $QUERY->execute([
+            'pwd' => $password,
+            'editUser' => $editUser,
+            'id' => $user
+        ]);
+        $QUERY->closeCursor();
+    }
+
+    public function UpdateLastConnectionTime(string $datetime, int $id) {
+        $REQUEST = 'UPDATE `access` SET derniereConnection = :derniereConnection WHERE `id` = :id';
+        $QUERY = Connection()->prepare($REQUEST);
+        $QUERY->execute([
+            'derniereConnection' => $datetime,
+            'id' => $id
+        ]);
+        $QUERY->closeCursor();
+    }
+
 }
